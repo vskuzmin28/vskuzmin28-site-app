25ec1c05d8c1149e63ae332ed6f446391c567b09
vskuzmin28 <vskuzmin28@gmail.com>
2024-08-10 20:34:29 +0300

    fixed icons, components

---
+++ src/components/ClientsScreen.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+.clients-screen 
+    .clients-screen.container
+        h2.clients-screen__title.title
+        ul.clients-screen__list 
+            li.clients-screen__item 
+                | 1
+            li.clients-screen__item 
+                | 1
+            li.clients-screen__item 
+                | 1
+</template>
+
+<style lang="scss">
+.clients-screen {
+}
+</style>
---
+++ src/components/icons/IconSquare.vue
@@ -0,0 +1,66 @@
+<template>
+  <svg
+    width="156"
+    height="180"
+    viewBox="0 0 156 180"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle cx="6" cy="174" r="6" transform="rotate(-90 6 174)" fill="#F6F9FF" />
+    <circle cx="30" cy="174" r="6" transform="rotate(-90 30 174)" fill="#F6F9FF" />
+    <circle cx="54" cy="174" r="6" transform="rotate(-90 54 174)" fill="#F6F9FF" />
+    <circle cx="78" cy="174" r="6" transform="rotate(-90 78 174)" fill="#F6F9FF" />
+    <circle cx="102" cy="174" r="6" transform="rotate(-90 102 174)" fill="#F6F9FF" />
+    <circle cx="126" cy="174" r="6" transform="rotate(-90 126 174)" fill="#F6F9FF" />
+    <circle cx="150" cy="174" r="6" transform="rotate(-90 150 174)" fill="#F6F9FF" />
+    <circle cx="6" cy="150" r="6" transform="rotate(-90 6 150)" fill="#F6F9FF" />
+    <circle cx="30" cy="150" r="6" transform="rotate(-90 30 150)" fill="#F6F9FF" />
+    <circle cx="54" cy="150" r="6" transform="rotate(-90 54 150)" fill="#F6F9FF" />
+    <circle cx="78" cy="150" r="6" transform="rotate(-90 78 150)" fill="#F6F9FF" />
+    <circle cx="102" cy="150" r="6" transform="rotate(-90 102 150)" fill="#F6F9FF" />
+    <circle cx="126" cy="150" r="6" transform="rotate(-90 126 150)" fill="#F6F9FF" />
+    <circle cx="150" cy="150" r="6" transform="rotate(-90 150 150)" fill="#F6F9FF" />
+    <circle cx="6" cy="126" r="6" transform="rotate(-90 6 126)" fill="#F6F9FF" />
+    <circle cx="30" cy="126" r="6" transform="rotate(-90 30 126)" fill="#F6F9FF" />
+    <circle cx="54" cy="126" r="6" transform="rotate(-90 54 126)" fill="#F6F9FF" />
+    <circle cx="78" cy="126" r="6" transform="rotate(-90 78 126)" fill="#F6F9FF" />
+    <circle cx="102" cy="126" r="6" transform="rotate(-90 102 126)" fill="#F6F9FF" />
+    <circle cx="126" cy="126" r="6" transform="rotate(-90 126 126)" fill="#F6F9FF" />
+    <circle cx="150" cy="126" r="6" transform="rotate(-90 150 126)" fill="#F6F9FF" />
+    <circle cx="6" cy="102" r="6" transform="rotate(-90 6 102)" fill="#F6F9FF" />
+    <circle cx="30" cy="102" r="6" transform="rotate(-90 30 102)" fill="#F6F9FF" />
+    <circle cx="54" cy="102" r="6" transform="rotate(-90 54 102)" fill="#F6F9FF" />
+    <circle cx="78" cy="102" r="6" transform="rotate(-90 78 102)" fill="#F6F9FF" />
+    <circle cx="102" cy="102" r="6" transform="rotate(-90 102 102)" fill="#F6F9FF" />
+    <circle cx="126" cy="102" r="6" transform="rotate(-90 126 102)" fill="#F6F9FF" />
+    <circle cx="150" cy="102" r="6" transform="rotate(-90 150 102)" fill="#F6F9FF" />
+    <circle cx="6" cy="78" r="6" transform="rotate(-90 6 78)" fill="#F6F9FF" />
+    <circle cx="30" cy="78" r="6" transform="rotate(-90 30 78)" fill="#F6F9FF" />
+    <circle cx="54" cy="78" r="6" transform="rotate(-90 54 78)" fill="#F6F9FF" />
+    <circle cx="78" cy="78" r="6" transform="rotate(-90 78 78)" fill="#F6F9FF" />
+    <circle cx="102" cy="78" r="6" transform="rotate(-90 102 78)" fill="#F6F9FF" />
+    <circle cx="126" cy="78" r="6" transform="rotate(-90 126 78)" fill="#F6F9FF" />
+    <circle cx="150" cy="78" r="6" transform="rotate(-90 150 78)" fill="#F6F9FF" />
+    <circle cx="6" cy="54" r="6" transform="rotate(-90 6 54)" fill="#F6F9FF" />
+    <circle cx="30" cy="54" r="6" transform="rotate(-90 30 54)" fill="#F6F9FF" />
+    <circle cx="54" cy="54" r="6" transform="rotate(-90 54 54)" fill="#F6F9FF" />
+    <circle cx="78" cy="54" r="6" transform="rotate(-90 78 54)" fill="#F6F9FF" />
+    <circle cx="102" cy="54" r="6" transform="rotate(-90 102 54)" fill="#F6F9FF" />
+    <circle cx="126" cy="54" r="6" transform="rotate(-90 126 54)" fill="#F6F9FF" />
+    <circle cx="150" cy="54" r="6" transform="rotate(-90 150 54)" fill="#F6F9FF" />
+    <circle cx="6" cy="30" r="6" transform="rotate(-90 6 30)" fill="#F6F9FF" />
+    <circle cx="30" cy="30" r="6" transform="rotate(-90 30 30)" fill="#F6F9FF" />
+    <circle cx="54" cy="30" r="6" transform="rotate(-90 54 30)" fill="#F6F9FF" />
+    <circle cx="78" cy="30" r="6" transform="rotate(-90 78 30)" fill="#F6F9FF" />
+    <circle cx="102" cy="30" r="6" transform="rotate(-90 102 30)" fill="#F6F9FF" />
+    <circle cx="126" cy="30" r="6" transform="rotate(-90 126 30)" fill="#F6F9FF" />
+    <circle cx="150" cy="30" r="6" transform="rotate(-90 150 30)" fill="#F6F9FF" />
+    <circle cx="6" cy="6" r="6" transform="rotate(-90 6 6)" fill="#F6F9FF" />
+    <circle cx="30" cy="6" r="6" transform="rotate(-90 30 6)" fill="#F6F9FF" />
+    <circle cx="54" cy="6" r="6" transform="rotate(-90 54 6)" fill="#F6F9FF" />
+    <circle cx="78" cy="6" r="6" transform="rotate(-90 78 6)" fill="#F6F9FF" />
+    <circle cx="102" cy="5.99999" r="6" transform="rotate(-90 102 5.99999)" fill="#F6F9FF" />
+    <circle cx="126" cy="5.99999" r="6" transform="rotate(-90 126 5.99999)" fill="#F6F9FF" />
+    <circle cx="150" cy="5.99999" r="6" transform="rotate(-90 150 5.99999)" fill="#F6F9FF" />
+  </svg>
+</template>
